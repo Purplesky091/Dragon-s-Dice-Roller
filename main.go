d29113f85dc0ca06f642079192df4b064c018c2b
purplesky091 <ray.hsia@gmail.com>
2026-06-25 11:33:54 -0500

    Put command line version of app into separate struct and have it called from main

--- main.go
+++ main.go
@@ -1,20 +1,19 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
 	"log/slog"
 	"os"
 )
 
 var opts = &slog.HandlerOptions{Level: slog.LevelInfo}
 var logger = slog.New(slog.NewTextHandler(os.Stdout, opts))
-var useDiscordBot = true
+var useDiscordBot = false
 
 func main() {
 	slog.SetDefault(logger)
 	if !useDiscordBot {
-		RunConsoleVersion()
+		cli := new(CLI)
+		cli.Run()
 		return
 	}
 
@@ -22,38 +21,5 @@ func main() {
 	if token == "" {
 		slog.Error("DISCORD_TOKEN env var not set.")
 		os.Exit(1)
-	}
-
-}
-
-func RunConsoleVersion() {
-	scanner := bufio.NewScanner(os.Stdin)
-	var input string
-	fmt.Print("Enter dice you wish to roll (format: <num>d<num>): ")
-	if scanner.Scan() {
-		input = scanner.Text()
-	}
-	slog.Debug("User input", "input", input)
-	dice, err := NewDice(input)
-
-	if err != nil {
-		fmt.Println("Error", err)
-		os.Exit(1)
-	}
-
-	diceRoll := dice.Roll()
-	fmt.Println(diceRoll)
-	winningRoll, advRolls := dice.RollAdvantage()
-
-	fmt.Println("Winning roll:", winningRoll.result)
-	for i := 0; i < len(advRolls); i++ {
-		fmt.Println(advRolls[i])
-	}
-
-	fmt.Println("Rolling disadvantage")
-	minRoll, disAdvRolls := dice.RollDisadvantage()
-	fmt.Println("Winning roll:", minRoll.result)
-	for i := 0; i < len(disAdvRolls); i++ {
-		fmt.Println(disAdvRolls[i])
 	}
 }
