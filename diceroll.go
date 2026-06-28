6a44668f37376e6108e8fcedba4fa69da0e8dfac
purplesky091 <ray.hsia@gmail.com>
2026-06-28 16:49:29 -0500

    Remove unused code

--- diceroll.go
+++ diceroll.go
@@ -15,17 +15,6 @@ func (diceRoll DiceRoll) String() string {
 	return fmt.Sprintf("rolls:%v\nsum:%d", diceRoll.rolls, diceRoll.result)
 }
 
-func (diceRoll DiceRoll) DiscordString() string {
-	result := ""
-	if len(diceRoll.rolls) <= MaxDisplayableRolls {
-		result = fmt.Sprintf("rolls: %v\nsum: %d", diceRoll.rolls, diceRoll.result)
-	} else {
-		result = fmt.Sprintf("sum: %d", diceRoll.result)
-	}
-
-	return result
-}
-
 func (diceRoll DiceRoll) writeRollsWrapped() string {
 	var builder strings.Builder
 	for index, roll := range diceRoll.rolls {
