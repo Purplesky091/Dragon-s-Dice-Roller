0e35194eab8ef32c8dfba46e22aa1d997db7ac3b
Lawrence Hsia <ray.hsia@gmail.com>
2026-07-01 03:59:55 -0500

    Refactor/cleanup newdice (#7)
    * created helper functions for getting dicecount and facecount
    
    * Put postaction parsing into helper method

--- dice.go
+++ dice.go
@@ -67,58 +67,18 @@ func NewDice(dice string) (Dice, error) {
 	if matchingErr != nil {
 		return Dice{}, matchingErr
 	}
-	var diceCount int = 1 // assume default of 1 in case first parameter is not provided (since the number before d is optional and if left blank means 1)
-	var faceCount int
 
-	if matches["DiceCount"] != "" {
-		var err error
-		diceCount, err = strconv.Atoi(matches["DiceCount"])
-		if err != nil {
-			return Dice{}, fmt.Errorf("Invalid dice count: %w", err)
-		}
-
-		if diceCount == 0 {
-			return Dice{}, fmt.Errorf("Number of dice must be greater than 0")
-		}
-
-		if diceCount > diceCountCap {
-			return Dice{}, fmt.Errorf("The dice count %d in %q is too high. Max is %d", diceCount, dice, diceCountCap)
-		}
+	diceCount, diceCountErr := getDiceCount(matches["DiceCount"], dice)
+	if diceCountErr != nil {
+		return Dice{}, diceCountErr
 	}
 
-	faceCount, err := strconv.Atoi(matches["FaceCount"])
-	if err != nil {
-		return Dice{}, fmt.Errorf("Invalid face count: %w", err)
+	faceCount, faceCountErr := getFaceCount(matches["FaceCount"])
+	if faceCountErr != nil {
+		return Dice{}, faceCountErr
 	}
 
-	if faceCount == 0 {
-		return Dice{}, fmt.Errorf("Can't roll a d0. Number of faces must be 1 or higher")
-	} else if faceCount > faceCap {
-		return Dice{}, fmt.Errorf("Can't roll a d%d. Max is d%d", faceCount, faceCap)
-	}
-
-	postApplyFlag := matches["PostApplyFlag"]
-	var postAction PostAction = nil
-
-	if postApplyFlag != "" {
-		var postApplyCount int = 1
-		slog.Info("PostApplyFlag set", "postApplyFlag", postApplyFlag)
-		if matches["PostApplyCount"] != "" {
-			postApplyCount, _ = strconv.Atoi(matches["PostApplyCount"])
-			slog.Info("PostApplyCount set", "PostApplyCount", postApplyCount)
-		} else {
-			slog.Info("No PostApplyCount set. Defaulting to 1")
-		}
-
-		switch postApplyFlag {
-		case "kh":
-			postAction = KeepHighest{keepCount: postApplyCount}
-		case "dl":
-			postAction = DropLowest{dropCount: postApplyCount}
-		default:
-			postAction = nil
-		}
-	}
+	postAction := getPostAction(matches["PostApplyFlag"], matches["PostApplyCount"])
 
 	return Dice{count: diceCount, faces: faceCount, postAction: postAction}, nil
 }
@@ -137,4 +97,73 @@ func makeSubmatchMap(regex *regexp.Regexp, inputString string) (map[string]strin
 	}
 
 	return subMatchMap, nil
+}
+
+func getDiceCount(diceCountStr string, diceStr string) (int, error) {
+	// assume default of 1 in case first parameter is not provided (since the number before d is optional and if left blank means 1)
+	if diceCountStr == "" {
+		return 1, nil
+	}
+
+	var err error
+	var diceCount int
+
+	diceCount, err = strconv.Atoi(diceCountStr)
+	if err != nil {
+		return 0, fmt.Errorf("Invalid dice count: %w", err)
+	}
+
+	if diceCount == 0 {
+		return 0, fmt.Errorf("Number of dice must be greater than 0")
+	}
+
+	if diceCount > diceCountCap {
+		return 0, fmt.Errorf("The dice count %d in %q is too high. Max is %d", diceCount, diceStr, diceCountCap)
+	}
+
+	return diceCount, nil
+}
+
+func getFaceCount(faceCountStr string) (int, error) {
+	faceCount, err := strconv.Atoi(faceCountStr)
+	if err != nil {
+		return 0, fmt.Errorf("Invalid face count: %w", err)
+	}
+
+	if faceCount == 0 {
+		return 0, fmt.Errorf("Can't roll a d0. Number of faces must be 1 or higher")
+	} else if faceCount > faceCap {
+		return 0, fmt.Errorf("Can't roll a d%d. Max is d%d", faceCount, faceCap)
+	}
+
+	return faceCount, nil
+}
+
+func getPostAction(postApplyFlag string, postApplyCountStr string) PostAction {
+	var postAction PostAction = nil
+	var postApplyCount int
+
+	if postApplyFlag == "" {
+		return nil
+	}
+
+	slog.Info("PostApplyFlag set", "postApplyFlag", postApplyFlag)
+	if postApplyCountStr != "" {
+		postApplyCount, _ = strconv.Atoi(postApplyCountStr)
+		slog.Info("PostApplyCount set", "PostApplyCount", postApplyCount)
+	} else {
+		postApplyCount = 1
+		slog.Info("No PostApplyCount set. Defaulting to 1")
+	}
+
+	switch postApplyFlag {
+	case "kh":
+		postAction = KeepHighest{keepCount: postApplyCount}
+	case "dl":
+		postAction = DropLowest{dropCount: postApplyCount}
+	default:
+		postAction = nil
+	}
+
+	return postAction
 }
